35cfd2a79b5f0ca0caf7ffee5ebea93f72c04062
Cratobi <ci.riyadh@gmail.com>
2019-01-05 08:01:08 +0600

    temp

--- fetcher/index.js
+++ fetcher/index.js
@@ -1,28 +1,42 @@
 const git = require('git-state')
+const shell = require('shelljs');
 const fs = require('fs')
 
 const path = '../'
 const fileLocation = 'temp.txt'
 
-git.isGit(path, async (exists) => {
+const saveFile = (location, data) => {
+    fs.writeFile(location, data, (err) => {
+        if (err) throw err
+    })
+}
+
+git.isGit(path, async(exists) => {
     if (!exists) return
 
-    const currentHash = await git.commitSync(path, (err, hash) => {
-        if (err) throw err
+    try {
+        shell.exec('sh ./scripts/git-pull.sh')
+        const currentHash = await git.commitSync(path, (err, hash) => {
+            if (err) throw err
 
-        return hash
-    })
-    const lastHash = await fs.readFileSync(fileLocation, (err, buf) => {
-        if (err) throw err
+            return hash
+        })
+        if (!await fs.existsSync(fileLocation)) {
+            saveFile(fileLocation, currentHash)
+        }
 
-        return buf
-    }).toString()
+        const lastHash = await fs.readFileSync(fileLocation, buf => buf).toString()
 
-    if(currentHash !== lastHash) {
-      console.log('script')
+        if (currentHash !== lastHash) {
+            console.log(`Updating | Time: ${new Date().getTime()}`)
+
+            shell.exec('run.sh')
+            // saveFile(fileLocation, currentHash)
+            // exec('sh server-reload.sh', err => {throw err})
+        }
+        
+
+    } catch (err) {
+        console.log(err)
     }
-    // fs.writeFile(fileLocation, currentHash, (err) => {
-    //     if (err) throw err
-    // })
-
 })
---
+++ fetcher/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hi There!2"
+sleep 1
