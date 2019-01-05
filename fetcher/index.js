a9c9f2730905167211b62d3f05942c75eb9e6adf
Cratobi <ci.riyadh@gmail.com>
2019-01-05 06:48:56 +0600

    temp

--- fetcher/index.js
+++ fetcher/index.js
@@ -1,13 +1,28 @@
-var git = require('git-state')
- 
-var path = '../'
- 
-git.isGit(path, (exists) => {
-  if (!exists) return
- 
-  git.commit(path, (err, r) => {
-    if (err) throw err
+const git = require('git-state')
+const fs = require('fs')
 
-    console.log(r)
-  })
+const path = '../'
+const fileLocation = 'temp.txt'
+
+git.isGit(path, async (exists) => {
+    if (!exists) return
+
+    const currentHash = await git.commitSync(path, (err, hash) => {
+        if (err) throw err
+
+        return hash
+    })
+    const lastHash = await fs.readFileSync(fileLocation, (err, buf) => {
+        if (err) throw err
+
+        return buf
+    }).toString()
+
+    if(currentHash !== lastHash) {
+      console.log('script')
+    }
+    // fs.writeFile(fileLocation, currentHash, (err) => {
+    //     if (err) throw err
+    // })
+
 })
