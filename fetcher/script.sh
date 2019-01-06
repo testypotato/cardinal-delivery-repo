1940bb5af73d535c65037006792f9566800fbb38
Cratobi <ci.riyadh@gmail.com>
2019-01-06 19:07:27 +0600

    V3.0

--- fetcher/script.sh
+++ fetcher/script.sh
@@ -10,6 +10,11 @@ if [ "$HEADHASH" != "$UPSTREAMHASH" ]
 then
 	git merge origin/master
 	rsync nginx.conf etc/nginx/nginx.conf
+	cd api
+	npm install
+	cd ../fetcher
+	npm install
 	systemctl reload nginx
 	pm2 reload api
+	pm2 reload fetcher
 fi
--- server-reload.sh
+++ server-reload.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+rsync nginx.conf etc/nginx/nginx.conf
+cd api
+npm install
+cd ../fetcher
+npm install
 systemctl reload nginx
 pm2 reload api
+pm2 reload fetcher
